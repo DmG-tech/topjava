35e6f3327c341cc6001e8acc30fafb95192b286c
Dima <dm.golubckov@mail.ru>
2020-06-16 09:12:57 +0300

    2_1_HW1.patch

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -3,8 +3,6 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealTo {
-    private final Integer id;
-
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -16,8 +14,7 @@ public class MealTo {
 //    private final Supplier<Boolean> excess;  // filteredByClosure
     private final boolean excess;
 
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
-        this.id = id;
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -27,10 +24,6 @@ public class MealTo {
 //    public Boolean getExcess() {
 //        return excess.get();
 //    }
-
-    public Integer getId() {
-        return id;
-    }
 
     public LocalDateTime getDateTime() {
         return dateTime;
@@ -51,8 +44,7 @@ public class MealTo {
     @Override
     public String toString() {
         return "MealTo{" +
-                "id=" + id +
-                ", dateTime=" + dateTime +
+                "dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
                 ", excess=" + excess +
