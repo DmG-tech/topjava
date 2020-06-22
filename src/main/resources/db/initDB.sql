f5286d2d4e42705f343e44ac0fcd55484a336674
Dima <dm.golubckov@mail.ru>
2020-06-22 15:33:23 +0300

    hw03 completed

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP table if exists meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -6,20 +7,31 @@ CREATE SEQUENCE global_seq START WITH 100000;
 
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name             VARCHAR                 NOT NULL,
-  email            VARCHAR                 NOT NULL,
-  password         VARCHAR                 NOT NULL,
-  registered       TIMESTAMP DEFAULT now() NOT NULL,
-  enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name             VARCHAR                           NOT NULL,
+    email            VARCHAR                           NOT NULL,
+    password         VARCHAR                           NOT NULL,
+    registered       TIMESTAMP           DEFAULT now() NOT NULL,
+    enabled          BOOL                DEFAULT TRUE  NOT NULL,
+    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
-  user_id INTEGER NOT NULL,
-  role    VARCHAR,
-  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    user_id INTEGER NOT NULL,
+    role    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+create table meals
+(
+    user_id     INTEGER                           NOT NULL,
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    description VARCHAR                           NOT NULL,
+    dateTime    TIMESTAMP           DEFAULT now() NOT NULL,
+    calories    INTEGER             DEFAULT 0     NOT NULL,
+    CONSTRAINT user_meal UNIQUE (user_id, id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+)
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,33 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
+DELETE
+FROM meals;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, datetime, calories)
+VALUES (100000, 'завтрак', '2020-02-20 09:05:30', 500),
+       (100000, 'обед', '2020-02-20 13:10:25', 500),
+       (100000, 'ужин', '2020-02-20 19:25:30', 500),
+
+       (100000, 'завтрак', '2020-03-20 10:05:30', 600),
+       (100000, 'обед', '2020-03-20 14:10:25', 700),
+       (100000, 'ужин', '2020-03-20 16:25:30', 800),
+
+       (100000, 'завтрак', '2020-04-20 08:05:30', 1000),
+       (100000, 'обед', '2020-04-20 12:10:25', 300),
+       (100000, 'ужин', '2020-04-20 20:25:30', 100),
+
+       (100000, 'завтрак', '2020-05-20 09:20:30', 800),
+       (100000, 'обед', '2020-05-20 13:40:25', 900),
+       (100000, 'ужин', '2020-05-20 19:50:30', 1000);
